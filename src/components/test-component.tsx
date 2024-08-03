eabe25bb6df4cfa06c708ffba580aca522289e9e
Ana Margarida Picoito <anamargaridapic8@gmail.com>
2024-08-03 19:02:29 +0100

    pwa-with-vercel

---
+++ src/components/test-component.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from '@mui/base/Button';
+
+export default function Home() {
+    return (
+        <div>
+            This is a test
+            <Button>Click Me</Button>
+        </div>
+    );
+}
